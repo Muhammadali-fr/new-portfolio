6f43bd69e449202a91ecce23d1b62711266bbad3
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-02 20:15:55 +0500

    portfolio

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -2,7 +2,7 @@ export default function About(){
     return(
         
         <div className="w-full min-h-screen flex items-center justify-center">
-            <div className="w-[95%] h-[95vh] bg-blue-900 rounded-lg">
+            <div className="w-[90%] h-[90vh] bg-blue-900 rounded-lg">
 
             </div>
         </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
+// componetns 
+import Sidebar from "./components/Sidebar";
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,7 +30,12 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div className="flex">
+          {/* Sidebar  */}
+          <Sidebar/>
+
+          {children}
+        </div>
       </body>
     </html>
   );
