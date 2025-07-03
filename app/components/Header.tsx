eecc9fc0cc032f7ff0ba5b805ac40b21c44615da
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-03 17:31:30 +0500

    header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,5 +1,27 @@
 export default function Header() {
   return (
-    <h1>Header</h1>
+    <div className="w-[70%] mx-auto flex items-center justify-between py-5">
+      <div className="flex items-center gap-2">
+        <img className="w-[70px] h-[70px] rounded-full object-top object-cover" src="/assets/my-image.jpeg" alt="my-image" />
+        <div>
+          <p className="font-semibold ">Muhammadali</p>
+          <p className="text-gray-600">Software Engineer</p>
+        </div>
+      </div>
+
+      <ul className="flex gap-5">
+        <li className="">
+          <a href="">Work</a>
+        </li>
+
+        <li>
+          <a href="">About</a>
+        </li>
+
+        <li>
+          <a href="">Contact</a>
+        </li>
+      </ul>
+    </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
 // componetns 
-import Sidebar from "./components/Header";
+import Header from "./components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Header/>
         {children}
       </body>
     </html>
