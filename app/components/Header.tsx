478f3bd58038c4749922a2bd0c9d9ea84eb84377
Muhammadali-fr <boburjonr934@gmail.com>
2025-07-04 11:17:16 +0500

    works page

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,27 +1,39 @@
+"use client"
+
+// lucide 
+import { Github } from "lucide-react"
+import Link from "next/link"
+
 export default function Header() {
   return (
-    <div className="w-[70%] mx-auto flex items-center justify-between py-5">
+    <div className="w-[90%] max-w-[990px] mx-auto flex items-center justify-between py-5">
+
       <div className="flex items-center gap-2">
-        <img className="w-[70px] h-[70px] rounded-full object-top object-cover" src="/assets/my-image.jpeg" alt="my-image" />
+        <img className="w-[60px] h-[60px] rounded-full object-top object-cover select-none" src="/assets/my-image.jpeg" alt="my-image" />
         <div>
           <p className="font-semibold ">Muhammadali</p>
           <p className="text-gray-600">Software Engineer</p>
         </div>
       </div>
 
-      <ul className="flex gap-5">
-        <li className="">
-          <a href="">Work</a>
+      <ul className="flex items-center gap-5">
+        <li className="hover:text-gray-500">
+          <a href="">work</a>
         </li>
 
-        <li>
-          <a href="">About</a>
+        <li className="hover:text-gray-500">
+          <a href="">about</a>
         </li>
 
-        <li>
-          <a href="">Contact</a>
+        <li className="hover:text-gray-500">
+          <a href="">contact</a>
         </li>
+
+        <Link target="_blank" href={"https://github.com/Muhammadali-fr"} className="w-[40px] h-[40px] border border-gray-300 rounded-lg hover:bg-gray-100 cursor-pointer flex items-center justify-center text-gray-700">
+          <Github/>
+        </Link>
       </ul>
+
     </div>
   )
 }
---
+++ data/data.js
@@ -0,0 +1,53 @@
+export const myWork = [
+  {
+    name: "Delizi",
+    image: "/works/delizi.png",
+    description: "This web site was created while learning react.",
+    netlifyLink: "https://benevolent-croissant-7f2247.netlify.app/",
+    gitHubLink: "https://github.com/Muhammadali-fr/Delizi",
+    creating: false,
+  },
+  {
+    name: "Car-scream",
+    image: "/works/car-scream.png",
+    description:
+      "The website wasn’t finished because I only wanted to learn about React Router.",
+    netlifyLink: "https://inquisitive-kelpie-c7c2ed.netlify.app/",
+    gitHubLink: "https://github.com/Muhammadali-fr/Car-scream",
+    creating: false,
+  },
+  {
+    name: "Instagram clone. (Back-end)",
+    image: "/works/ig_clone.png",
+    description:
+      "The web site created for my friend's react exam. So I help him for back end.",
+    gitHubLink: "https://github.com/Alisher009Nabijonov/Instagram",
+    creating: false,
+  },
+  {
+    name: "Dast images.",
+    image: "/works/dast.png",
+    description:
+      "The web site making for show my skills and I really like this site design.",
+    netlifyLink: "https://dast-pictures.netlify.app/",
+    gitHubLink: "https://github.com/Muhammadali-fr/dast_client",
+    creating: false,
+  },
+  {
+    name: "Login/Sign up.",
+    image: "/works/round.png",
+    description:
+      "The web site created jsut for fun and UI.",
+    netlifyLink: "https://sign-up-login-psi.vercel.app/",
+    gitHubLink: "https://github.com/Muhammadali-fr/sign-up-login-",
+    creating: false,
+  },
+  {
+    name: "Nuna",
+    image: "/works/nuna.png",
+    description:
+      "This social media is creating using Next Js, Nest Js and Redux for clean structure.",
+    gitHubLink: "https://github.com/nuna82",
+    creating: true,
+  },
+];
